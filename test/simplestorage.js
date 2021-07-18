74839757d62d3905877af7dd2a9907ee6fab0169
Ivan Antolic-Soban <ivan.soban@gmail.com>
2021-07-18 14:30:01 -0400

    Adding BattleScape contract.

--- test/simplestorage.js
+++ test/simplestorage.js
@@ -1,15 +1,23 @@
-const SimpleStorage = artifacts.require("SimpleStorage");
+const BattleScape = artifacts.require("BattleScape");
+const EncountrToken = artifacts.require("Encountr");
 
-contract("SimpleStorage", accounts => {
-  it("...should store the value 89.", async () => {
-    const simpleStorageInstance = await SimpleStorage.deployed();
+contract("BattleScape", accounts => {
 
-    // Set value of 89
-    await simpleStorageInstance.set(89, { from: accounts[0] });
+  it("... should add a wager to the ledger.", async () => {
+    const battleScapeInstance = await BattleScape.deployed();
+    const ecountrInstance = await EncountrToken.deployed();
 
-    // Get stored value
-    const storedData = await simpleStorageInstance.storedData.call();
+    const evt = accounts[1];
+    const outcome = 1;
+    const wagerAmount = 10;
 
-    assert.equal(storedData, 89, "The value 89 was not stored.");
+    await ecountrInstance.increaseAllowance(battleScapeInstance.address, wagerAmount);
+    await battleScapeInstance.wager(evt, outcome, wagerAmount);
+
+    const w = await battleScapeInstance.getWager(evt);
+
+    assert(web3.utils.toBN(outcome).eq(w[0]), "The outcomes must be equal");
+    assert(web3.utils.toBN(wagerAmount).eq(w[1]), "The amounts must be equal");
   });
+
 });
